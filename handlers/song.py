ae5308792e524bd73aff2f09b5ad3508352ad5ef
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-10-16 05:05:30 +0400

    update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -45,7 +45,7 @@ def bul(client, message):
 
     except Exception as e:
         m.edit(
-            "❌ Mahnı tapılmadı.\n\nBaşqa mahnı yazın, və ya mahnı adı düzgün deyil."
+            "❌ Mahnı tapılmadı.\n\nBaşqa mahnı yazın, və ya mahnı adı düzgün deyil ‼️"
         )
         print(str(e))
         return
@@ -55,7 +55,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☑️ **Başlıq**: [{title[:35]}]({link})\n🎬 **Mənbə**: YouTube\n⏱️ **Müddət**: `{duration}`\n👁‍🗨 **Baxış Sayı**: `{views}`\n📤 **Yükləyən**: @{BOT_USERNAME}"
+        rep = f"🎵 **Başlıq**: [{title[:35]}]({link})\n🎬 **Mənbə**: YouTube\n⏱️ **Müddət**: `{duration}`\n👁‍🗨 **Baxış Sayı**: `{views}`\n🤖 **Yükləyən**: @{BOT_USERNAME}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -114,7 +114,7 @@ async def vsong(client, message):
     except Exception as e:
         return await msg.edit(f"🚫 **Xəta:** {e}")
     preview = wget.download(thumbnail)
-    await msg.edit("📤 **Video yüklənir...**")
+    await msg.edit("📥 **Video yüklənir...**")
     await message.reply_video(
         file_name,
         duration=int(ytdl_data["duration"]),
