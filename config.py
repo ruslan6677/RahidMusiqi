28cab121107ce19087a349d330b29ce6c2eabe5b
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2022-12-11 10:55:55 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ que = {}
 admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("5751318375:AAFYAM2_5qq58z0Lm-nd0klQn_q35J49nTE")
-BOT_NAME = getenv("BOT_NAME", "Chimney Music") 
+BOT_TOKEN = getenv("5602141301:AAFIPIdCBI5MeJaxA9kq9OvDoleubUYvKws")
+BOT_NAME = getenv("LogicalMusic) 
 API_ID = int(getenv("API_ID", "10300036"))
 API_HASH = getenv("79c295e05c970ddd724f0762ba275104")
-BOT_USERNAME = getenv("BOT_USERNAME", "CmineyMusicalbot")
+BOT_USERNAME = getenv("LogicalMusicalbot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
